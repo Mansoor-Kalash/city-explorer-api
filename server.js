3c08cadc1f08a92b8b99b46956ec7ab4104cccf6
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-08-18 07:47:14 +0300

    lab-07 BackEnd

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 'use strict';
+
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+const weatherD = require('./data/weather.json');
 
 const server = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
+server.use(cors());
 
-server.get('/test',(request,response)=>{
+server.get('/getweather',(req,res)=>{
+  
+
+    let city = req.query.cityName;
+    console.log("sss",city);
+
+    let weather =  weatherD.find(element => {
+
+        if (element.city_name.toLowerCase() === req.query.cityName.toLowerCase()) {
+        console.log('object :>> ', element);
+           return element;
+        }
+
+    })
+return weather;
+    res.send(wether,"aaa");
 
 })
+
+
+
+server.get('/',(req,res)=>{
+    res.send('home route');
+})
+
+
+
+
+server.get('*',(req,res)=>{
+    res.status(404).send('not found')
+})
+
+
 
 server.listen(PORT,()=>{
-    console.log(`${PORT}`);
+    console.log(`Listning on PORT ${PORT}`)
 })
-
