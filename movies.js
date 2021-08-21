4adedc85f8734a6d29a696bc01824ac1136442ce
Mansoor-Kalashe <mansoor.jow@gmaile.com>
2021-08-22 00:21:44 +0300

    lab-10 compleated

--- movies.js
+++ movies.js
@@ -1,38 +1,41 @@
-const axios = require('axios');
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 module.exports = getMoviData;
-let moviarr =[];
+let moviarr = [];
+let dataCash = {};
 
-function getMoviData(req,res){
-    let moviQuery = req.query.moviName;
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVI_API_KEY}&query=${moviQuery}=&language=en-US`;
+function getMoviData(req, res) {
+  let moviQuery = req.query.moviName;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVI_API_KEY}&query=${moviQuery}=&language=en-US`;
+  if (dataCash[moviQuery] !== undefined) {
+    res.send(dataCash[moviQuery]);
+  } else {
     try {
-        axios.get(url).then((resultData)=>{
-            console.log('movies',resultData);
-            let moviObj =resultData.data.results.map(element=>{
-               if (element.poster_path !== null) {
-                return new Movies (element);
-                   
-               }
-            })
-            res.send(moviarr);
-    
-        })
+      axios.get(url).then((resultData) => {
+        console.log("movies", resultData);
+        let moviObj = resultData.data.results.map((element) => {
+          if (element.poster_path !== null) {
+            return new Movies(element);
+          }
+        });
+        dataCash[moviQuery] = moviarr;
+        res.send(moviarr);
+      });
     } catch (error) {
-        console.log('error from axios', error)
-        res.send(error)
+      console.log("error from axios", error);
+      res.send(error);
     }
+  }
 }
-        
-    class Movies{
-        constructor(moviesData){
-            this.title=moviesData.title;
-            this.overview=moviesData.overview;
-            this.vote_average=moviesData.vote_average;
-            this.poster_path=moviesData.poster_path;
-            this.popularity=moviesData.popularity;
-            this.release_date=moviesData.release_date;
-            moviarr.push(this) 
-         }
-    
-    }
+
+class Movies {
+  constructor(moviesData) {
+    this.title = moviesData.title;
+    this.overview = moviesData.overview;
+    this.vote_average = moviesData.vote_average;
+    this.poster_path = moviesData.poster_path;
+    this.popularity = moviesData.popularity;
+    this.release_date = moviesData.release_date;
+    moviarr.push(this);
+  }
+}
--- server.js
+++ server.js
@@ -1,76 +1,59 @@
-'use strict';
+"use strict";
 
 // https://api.themoviedb.org/3/search/movie?api_key=${MOVI_API_KEY}&query=${CITY}=&language=en-US
 
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const axios = require("axios");
 
 const PORT = process.env.PORT;
 
 const server = express();
 server.use(cors());
-const weatherD = require('./data/weather.json');
-const getWeatherHandler = require('./weather.js');
-const getMoviData = require('./movies.js');
+const weatherD = require("./data/weather.json");
+const getWeatherHandler = require("./weather.js");
+const getMoviData = require("./movies.js");
 
 // ROUTES
 // lab-07
-server.get('/getweather',(req,res)=>{
-  
-try {
+server.get("/getweather", (req, res) => {
+  try {
     let city = req.query.cityName;
-    console.log("sss",city);
+    console.log("sss", city);
 
-    let weather =  weatherD.find(element => {
-
-        if (element.city_name.toLowerCase() === city.toLowerCase()) {
-        console.log('object :>> ', element);
-           return element;
-        }
-
-    })
+    let weather = weatherD.find((element) => {
+      if (element.city_name.toLowerCase() === city.toLowerCase()) {
+        console.log("object :>> ", element);
+        return element;
+      }
+    });
 
     res.send(weather);
     console.log(weather);
-
-} catch (error) {
-    console.log('error from axios', error)
-        res.send(error)
-}
-   
-})
+  } catch (error) {
+    console.log("error from axios", error);
+    res.send(error);
+  }
+});
 // lab-08
-server.get('/weather', getWeatherHandler);
-server.get(`/movies`,getMoviData);
+server.get("/weather", getWeatherHandler);
+server.get(`/movies`, getMoviData);
 
-
-
-
-server.get('/', homeHandler);
-server.get('*', notFoundHandler);
-
+server.get("/", homeHandler);
+server.get("*", notFoundHandler);
 
 // Function Handlers
 function homeHandler(req, res) {
-    res.send('all good')
+  res.send("all good");
 }
-
-
-
-
-
-
 
 function notFoundHandler(req, res) {
-    res.status(404).send({
-        "error": "Unable to get the route"
-    })
+  res.status(404).send({
+    error: "Unable to get the route",
+  });
 }
 
-
-
 server.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
-})
+  console.log(`Listening on PORT ${PORT}`);
+});
